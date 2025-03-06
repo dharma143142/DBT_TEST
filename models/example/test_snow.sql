310c1fe35dbcdf5a3393264dbdebe2b369122fd5
dharmendram.in@mouritech.com <dharmendram.in@mouritech.com>
2025-03-06 18:16:12 +0000

    msg

--- models/example/test_snow.sql
+++ models/example/test_snow.sql
@@ -2,4 +2,7 @@ select 1 emp_id ,'dharma' emp_name,'hyd' city
 
 select current_database(),current_schema()
 
-insert into test_snow values(2, 'mani','knl'),(3,'mohan','kdp'),(4,'sai','viz');
+{{config(materalized='view',schema='sample_schema')}}
+
+
+
