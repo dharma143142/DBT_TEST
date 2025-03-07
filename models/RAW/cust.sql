deb1e1c3aa58d31412c0e4e84b0dcaa9b57c9ca4
Dharmendra Reddy Mummadi <dharmendram.in@mouritech.com>
2025-03-07 07:04:49 +0000

    msg

---
+++ models/RAW/cust.sql
@@ -0,0 +1,11 @@
+SELECT
+
+ customer_id ,
+    customer_name,
+    email STRING,
+    phone_number
+    FROM SAMPLE_DB1.SAMPLE_DB1_SCHEMA.CUSTOMER
+
+    {{
+    config(materialized='table')
+    }}
