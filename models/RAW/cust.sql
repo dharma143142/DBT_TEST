e423a4c2fac75d781460445288e8953944041c94
Dharmendra Reddy Mummadi <dharmendram.in@mouritech.com>
2025-03-07 07:13:36 +0000

    msg

--- models/RAW/cust.sql
+++ models/RAW/cust.sql
@@ -6,6 +6,6 @@ SELECT
     phone_number
     FROM SAMPLE_DB1.SAMPLE_DB1_SCHEMA.CUSTOMER
 
-    {{
+   {{
     config(materialized='table')
     }}
--- models/RAW/product.sql
+++ models/RAW/product.sql
@@ -1,12 +1,11 @@
--- select 
+select 
 
--- product_id ,
---     product_name ,
---     category ,
---     price ,
---     stock_quantity
---     from SAMPLE_DB1.SAMPLE_DB1_SCHEMA.PRODUCT
-
-   {{
+product_id ,
+    product_name ,
+    category ,
+    price ,
+    stock_quantity
+    from SAMPLE_DB1.SAMPLE_DB1_SCHEMA.PRODUCT
+ {{
     config(materialized='table')
     }}
