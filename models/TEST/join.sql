634305068c9d2fe494c39936d5be8e1e2c49f622
Dharmendra Reddy Mummadi <dharmendram.in@mouritech.com>
2025-03-10 07:35:05 +0000

    msg

---
+++ models/TEST/join.sql
@@ -0,0 +1,10 @@
+SELECT 
+    ih.InvoiceID,
+    ih.CustomerID,
+    ih.InvoiceTotal,
+    il.ProductID,
+    il.Quantity,
+    il.LineTotal
+FROM {{ source('SAMPLE_DB1_SCHEMA', 'InvoiceHeader') }} ih
+JOIN {{ source('SAMPLE_DB2_SCHEMA', 'InvoiceLine') }} il
+    ON ih.InvoiceID = il.InvoiceID;
